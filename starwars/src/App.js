3ceef1d4300d0645aaf1c8ca8c2e249147e80265
Jose <jgonzalezmn@gmail.com>
2019-06-22 14:43:18 -0400

    mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Chloop from './components/loop'
 
 class App extends Component {
   constructor() {
@@ -33,6 +34,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <Chloop charaterInfo={this.state} />
+        
       </div>
     );
   }
